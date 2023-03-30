62b2bef6c0eca9dc09151fc00d6b55fd8c91c123
tarkvarakool <tarkvarakool@epost.ee>
2023-03-30 11:43:03 +0300

    All methods except main changed to private.

--- src/HttpClientTutorial.java
+++ src/HttpClientTutorial.java
@@ -31,6 +31,51 @@ public class HttpClientTutorial {
         }
     }
 
+    private static void login() throws IOException, InterruptedException {
+        System.out.println("Please type your username to log in: ");
+        String username = s.nextLine();
+        HttpResponse<String> response = doRequest("/login", username, "POST");
+        sessionId = response.headers().allValues("X-SESSION-ID").get(0);
+        if (response.statusCode() == 200) System.out.println("You are successfully logged in!");
+    }
+
+    private static void getStats() throws IOException, InterruptedException {
+        HttpResponse<String> response = doRequest("/stats", "", "GET");
+        System.out.println(response.body());
+    }
+
+    private static void getStatus() throws IOException, InterruptedException {
+        HttpResponse<String> response = doRequest("/status", "", "GET");
+        System.out.println(response.body());
+    }
+
+    private static void startGame() throws IOException, InterruptedException {
+        HttpResponse<String> response = doRequest("/start-game", "", "POST");
+        if (response.statusCode() == 200) {
+            System.out.println("Game started! Guess a number from 1 to 100!");
+        } else if (response.statusCode() == 401) {
+            offerToStartNewGame();
+        } else {
+            System.out.println("The game has already been started!");
+        }
+    }
+
+    private static void endGame() throws IOException, InterruptedException {
+        HttpResponse<String> response = doRequest("/end-game", "", "POST");
+        if (response.statusCode() == 200) {
+            System.out.println("Game ended!");
+        } else if (response.statusCode() == 401) {
+            offerToStartNewGame();
+        } else {
+            System.out.println("There is no active game!  Type 'start' for a new game");
+        }
+    }
+
+    private static void shutDownServer() {
+        System.out.println("Thank you for travelling with Linux express!");
+        System.exit(0);
+    }
+
     private static void getHelp() {
         System.out.println(
                 """
@@ -46,57 +91,7 @@ public class HttpClientTutorial {
         );
     }
 
-    private static void login() throws IOException, InterruptedException {
-        System.out.println("Please type your username to log in: ");
-        String username = s.nextLine();
-        HttpResponse<String> response = doRequest("/login", username, "POST");
-        sessionId = response.headers().allValues("X-SESSION-ID").get(0);
-        if (response.statusCode() == 200) System.out.println("You are successfully logged in!");
-    }
-
-    public static void getStats() throws IOException, InterruptedException {
-        HttpResponse<String> response = doRequest("/stats", "", "GET");
-        System.out.println(response.body());
-    }
-
-    public static void getStatus() throws IOException, InterruptedException {
-        HttpResponse<String> response = doRequest("/status", "", "GET");
-        System.out.println(response.body());
-    }
-
-    public static void startGame() throws IOException, InterruptedException {
-        HttpResponse<String> response = doRequest("/start-game", "", "POST");
-        if (response.statusCode() == 200) {
-            System.out.println("Game started! Guess a number from 1 to 100!");
-        } else if (response.statusCode() == 401) {
-            offerToStartNewGame();
-        } else {
-            System.out.println("The game has already been started!");
-        }
-    }
-
-    public static void endGame() throws IOException, InterruptedException {
-        HttpResponse<String> response = doRequest("/end-game", "", "POST");
-        if (response.statusCode() == 200) {
-            System.out.println("Game ended!");
-        } else if (response.statusCode() == 401) {
-            offerToStartNewGame();
-        } else {
-            System.out.println("There is no active game!  Type 'start' for a new game");
-        }
-    }
-
-    private static void offerToStartNewGame() throws IOException, InterruptedException {
-        System.out.println("Your current game session is expired. Would you like to start new one? (Y/N)");
-        if (s.nextLine().equalsIgnoreCase("Y")) {
-            login();
-            startGame();
-        } else {
-            shutDownServer();
-        }
-    }
-
-    public static void doGuess(String userInput) throws IOException, InterruptedException {
+    private static void doGuess(String userInput) throws IOException, InterruptedException {
         HttpResponse<String> response = doRequest("/guess", userInput, "POST");
         if (response.statusCode() == 200) {
             if (response.body().contains("EQUAL")) {
@@ -111,9 +106,14 @@ public class HttpClientTutorial {
         }
     }
 
-    private static void shutDownServer() {
-        System.out.println("Thank you for travelling with Linux express!");
-        System.exit(0);
+    private static void offerToStartNewGame() throws IOException, InterruptedException {
+        System.out.println("Your current game session is expired. Would you like to start new one? (Y/N)");
+        if (s.nextLine().equalsIgnoreCase("Y")) {
+            login();
+            startGame();
+        } else {
+            shutDownServer();
+        }
     }
 
     private static HttpResponse<String> doRequest(String endPoint, String body, String requestMethod) throws IOException, InterruptedException {
